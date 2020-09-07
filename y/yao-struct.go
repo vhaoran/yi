4781202705eb114bf30bbab63f83fb589d3d8f80
whr <whr@whr.local>
2020-09-07 17:04:12 +0800

    auto commit

--- y/yao-struct.go
+++ y/yao-struct.go
@@ -44,7 +44,7 @@ var Gong8WuXing2LiuQin = map[string]map[string]string{
 
 type Gua64 struct {
 	//所属宫
-	Gong string `json:"gong,omitempty"`
+	Gong string `json:"gong"`
 	//宫内顺序
 	GongOrder int `json:"gong_order"`
 	//所属五行
@@ -54,7 +54,7 @@ type Gua64 struct {
 	//短名称
 	NameS string `json:"name_short"`
 	//六爻(包含了世应)
-	L []string `json:"l,omitempty"`
+	L []string `json:"l"`
 }
 
 func NewGua64(gong string, gongOrder int, wuxing, name, names string, l ...string) *Gua64 {
